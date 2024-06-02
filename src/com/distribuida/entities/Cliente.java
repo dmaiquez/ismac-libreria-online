5075a3ca5b4e059042774aa429675237fee9776f
diegoalejo147852 <diegoalejo147852@gmail.com>
2024-06-01 20:30:51 -0500

    DMv0008 Patron DAO y Patron IoC en clase Cliente

--- src/com/distribuida/entities/Cliente.java
+++ src/com/distribuida/entities/Cliente.java
@@ -2,22 +2,40 @@ package com.distribuida.entities;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import org.springframework.stereotype.Component;
 
 
 @Component
+@Entity
+@Table(name = "cliente" )
 public class Cliente {
 	
 	
 	// estoy empezando a trabajar con el git y git hub
 	
 	// Atributos
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_cliente")
 	private int idCliente;
+	@Column(name = "cedula")
 	private String cedula;
+	@Column(name = "nombre")
 	private String nombre;
+	@Column(name = "apellido")
 	private String apellido;
+	@Column(name = "direccion")
 	private String direccion;
+	@Column(name = "telefono")
 	private String telefono;
+	@Column(name = "correo")
 	private String correo;
 	
 	
---
+++ src/com/distribuida/principal/PrincipalAutor.java
@@ -0,0 +1,10 @@
+package com.distribuida.principal;
+
+public class PrincipalAutor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
