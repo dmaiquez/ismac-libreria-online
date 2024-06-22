72cef765884441e8796b2d7f139ed7f2b1ac956f
diegoalejo147852 <diegoalejo147852@gmail.com>
2024-06-22 09:56:48 -0500

    DMv0009 Implementación de CRUD completo en ClienteDAOImpl con metodos automaticos de Hibernate. Implementación en AutorDAOImpl con HQL.

--- src/com/distribuida/principal/PrincipalAutor.java
+++ src/com/distribuida/principal/PrincipalAutor.java
@@ -1,10 +1,39 @@
 package com.distribuida.principal;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.distribuida.dao.AutorDAO;
+import com.distribuida.entities.Autor;
+
 public class PrincipalAutor {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		// Patron de IoC Inversión de Control
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");		
+		AutorDAO autorDAO = context.getBean("autorDAOImpl", AutorDAO.class);
+		
+		
+		// CRUD
+		// add
+		Autor autor = new Autor(0,"autor1","apellido1","pais1","direccion1","telefono1","correo1");
+		//autorDAO.add(autor);		
+		// up
+		Autor autor2 = new Autor(54,"autor2","apellido2","pais2","direccion2","telefono2","correo2");
+		//autorDAO.up(autor2);				
+		// del
+		autorDAO.del(54);
+		// findOne
+		//System.out.println("**************** DEL ********************* "+autorDAO.findOne(54));
+		try {System.out.println("**************** DEL ********************* "+autorDAO.findOne(54)); } catch(Exception e) { e.printStackTrace();}		
+		// find All		
+		autorDAO.findAll().forEach(item -> { System.out.println(item.toString()); });
+		
+				
+		context.close();
+		
 	}
 
 }
--- src/com/distribuida/principal/PrincipalCliente.java
+++ src/com/distribuida/principal/PrincipalCliente.java
@@ -15,17 +15,26 @@ public class PrincipalCliente {
 
 		
 		// Patron de IoC Inversión de Control
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
-		
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");		
 		ClienteDAO clienteDAO = context.getBean("clienteDAOImpl", ClienteDAO.class);
 		
-		List<Cliente> clientes = clienteDAO.findAll();
 		
+		// CRUD
+		// add
+		Cliente cliente = new Cliente(0,"1759638659", "juan", "taipe","av. por ahi y mas alla.","099863253","jtaipe@correo.com");
+		clienteDAO.add(cliente);		
+		// up
+		Cliente cliente2 = new Cliente(40, "1759638652", "juan2", "taipe2","av. por ahi y mas alla.2","099863252","jtaipe2@correo.com");
+		clienteDAO.up(cliente2);				
+		// del
+		clienteDAO.del(40);
+		// findOne
+		//System.out.println("**************** DEL ********************* "+clienteDAO.findOne(39));
+		try {System.out.println("**************** DEL ********************* "+clienteDAO.findOne(40)); } catch(Exception e) { e.printStackTrace();}		
+		// find All		
+		clienteDAO.findAll().forEach(item -> { System.out.println(item.toString()); });
 		
-		clientes.forEach(item -> {			
-			System.out.println(item.toString());
-		});
-		
+				
 		context.close();
 		
 	}
